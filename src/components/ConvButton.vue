c558b4fdb5406747ca7ed2473f7c6510a1fc29c6
hijiki02 <rebanira02@gmail.com>
2021-05-02 22:08:21 +0900

    Add ConvButton

---
+++ src/components/ConvButton.vue
@@ -0,0 +1,29 @@
+<template>
+	<button
+		@click="decode"
+		:class="`conv-button button ${color} is-flex-direction-column`"
+	>
+		<i :class="`fas fa-arrow-${direction} fa-3x`"></i>
+		<span>{{ label }}</span>
+	</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+	name: "ConvButton",
+	props: {
+		label: String,
+		direction: String,
+		color: String
+	}
+})
+</script>
+
+<style lang="scss" scoped>
+.conv-button {
+	width: 128px;
+	height: 128px;
+}
+</style>
