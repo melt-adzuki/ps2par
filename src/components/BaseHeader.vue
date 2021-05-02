22f9da2b64ffd40e146f7db43e5373d491cb02db
hijiki02 <rebanira02@gmail.com>
2021-05-02 23:18:47 +0900

    Separate to BaseHeader.vue

---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,12 @@
+<template>
+	<header class="hero is-primary">
+		<div class="container">
+			<div class="hero-body">
+				<p class="title">PS2PAR変換</p>
+				<p class="subtitle">
+					PS2PARコードを暗号化・複合化したり、PCSX2で使用できるpnachコードに変換したりできます。
+				</p>
+			</div>
+		</div>
+	</header>
+</template>
