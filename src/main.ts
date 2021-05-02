3ea8d2335ec96e988f8999d26d75634069573efe
hijiki02 <rebanira02@gmail.com>
2021-05-03 02:19:36 +0900

    Feature: Add toast notifications

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,29 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
+import { createApp } from "vue"
+import App from "./App.vue"
+import "./registerServiceWorker"
+import VueToastification, { TYPE } from "vue-toastification"
+import "vue-toastification/dist/index.css"
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(VueToastification, {
+	toastDefaults: {
+		[TYPE.SUCCESS]: {
+			position: "top-center",
+			timeout: 5000,
+			closeOnClick: true,
+			pauseOnFocusLoss: false,
+			pauseOnHover: false,
+			draggable: true,
+			draggablePercent: 0.6,
+			showCloseButtonOnHover: false,
+			hideProgressBar: true,
+			closeButton: "button",
+			icon: true,
+			rtl: false
+		}
+	},
+	transition: "Vue-Toastification__slideBlurred",
+	maxToasts: 2,
+	newestOnTop: true
+})
+app.mount("#app")
