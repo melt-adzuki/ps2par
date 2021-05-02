6ef1eac1fa7f3a84d99db261dfd519e36154a1f3
hijiki02 <rebanira02@gmail.com>
2021-05-02 22:26:55 +0900

    Separate footer

---
+++ src/components/BaseFooter.vue
@@ -0,0 +1,28 @@
+<template>
+	<footer class="footer">
+		<div class="content">
+			<p class="is-size-3">参考</p>
+			<p>こちらのサイトを参考に作らせていただきました。</p>
+			<ul>
+				<li>
+					<a href="https://ps2par-pnach.blogspot.com/"
+						>PS2PARコード→pnach用コード変換スクリプト</a
+					>
+				</li>
+				<li>
+					<a href="http://www.big.or.jp/~dram/ps2code.html"
+						>PS2PARコード変換スクリプト</a
+					>
+				</li>
+			</ul>
+		</div>
+	</footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+	name: "Footer"
+})
+</script>
