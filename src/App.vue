be38428dc24b2488b09bf25e9e1573f9a8f08cd8
hijiki02 <rebanira02@gmail.com>
2021-05-02 22:56:20 +0900

    Fix the expression

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 		<div class="hero-body">
 			<p class="title">PS2PAR変換</p>
 			<p class="subtitle">
-				PS2PARコードを暗号化・複合化したり、PCSX2で使用できるpnachコードに変換したりすることができます。
+				PS2PARコードを暗号化・複合化したり、PCSX2で使用できるpnachコードに変換したりできます。
 			</p>
 		</div>
 	</header>
