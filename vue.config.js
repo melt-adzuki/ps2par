78e3b8250b2f0939f86a52152b684d61c3611a5f
hijiki02 <rebanira02@gmail.com>
2021-05-03 01:56:04 +0900

    Change favicon

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
 	publicPath: process.env.NODE_ENV === "production" ? "/ps2par/" : "/",
+	pwa: {
+		name: "PS2PAR変換",
+		iconPaths: {
+			favicon32: "favicon.svg",
+			favicon16: null,
+			appleTouchIcon: "favicon.svg",
+			maskIcon: "favicon.svg",
+			msTileImage: "favicon.svg"
+		}
+	}
 }
