95784737037fbe90311af9d117227870309dde8d
hijiki02 <rebanira02@gmail.com>
2021-05-02 22:26:55 +0900

    Separate to BaseFooter.vue

--- src/App.vue
+++ src/App.vue
@@ -71,30 +71,13 @@
 			</div>
 		</div>
 	</section>
-
-	<footer class="footer">
-		<div class="content">
-			<p class="is-size-3">参考</p>
-			<p>こちらのサイトを参考に作らせていただきました。</p>
-			<ul>
-				<li>
-					<a href="https://ps2par-pnach.blogspot.com/"
-						>PS2PARコード→pnach用コード変換スクリプト</a
-					>
-				</li>
-				<li>
-					<a href="http://www.big.or.jp/~dram/ps2code.html"
-						>PS2PARコード変換スクリプト</a
-					>
-				</li>
-			</ul>
-		</div>
-	</footer>
+	<base-footer />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue"
 import ConvButton from "./components/ConvButton.vue"
+import BaseFooter from "./components/BaseFooter.vue"
 
 export default defineComponent({
 	name: "App",
@@ -216,6 +199,7 @@ export default defineComponent({
 
 			return result.trim()
 		}
+		BaseFooter
 	}
 })
 </script>
