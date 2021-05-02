aab4ca38572340d7ae31ead612797e95594fcc8b
hijiki02 <rebanira02@gmail.com>
2021-05-02 18:32:59 +0900

    Updates

---
+++ precache-manifest.61d8975fdb42b4119810b6087a8243b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cee2f88eb1aa73f994dd",
+    "url": "/ps2par/css/app.530f02f3.css"
+  },
+  {
+    "revision": "c4530462794be27b377f2f297a687b83",
+    "url": "/ps2par/index.html"
+  },
+  {
+    "revision": "cee2f88eb1aa73f994dd",
+    "url": "/ps2par/js/app.a5399441.js"
+  },
+  {
+    "revision": "0f0da0ae3fc3da74032c",
+    "url": "/ps2par/js/chunk-vendors.ed441dd5.js"
+  },
+  {
+    "revision": "6e718395ddc38a19a70411717be8d613",
+    "url": "/ps2par/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ps2par/robots.txt"
+  }
+]);
