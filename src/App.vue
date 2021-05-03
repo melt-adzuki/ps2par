d4be152e0fee29066e48e382cd4c7c140713b160
hijiki02 <rebanira02@gmail.com>
2021-05-04 02:03:26 +0900

    Improve the structure

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <template>
 	<base-header />
-	<section class="section has-bg-image">
-		<converter />
-	</section>
-	<base-footer />
+	<main>
+		<converter class="has-bg-image" />
+		<section class="section has-background-white-bis">
+			<div class="container">
+				<p class="is-size-3">参考</p>
+				<p>こちらのサイトを参考に作らせていただきました。</p>
+				<ul>
+					<li>
+						<a href="https://ps2par-pnach.blogspot.com/"
+							>PS2PARコード→pnach用コード変換スクリプト</a
+						>
+					</li>
+					<li>
+						<a href="http://www.big.or.jp/~dram/ps2code.html"
+							>PS2PARコード変換スクリプト</a
+						>
+					</li>
+				</ul>
+			</div>
+		</section>
+	</main>
+	<!-- <base-footer /> -->
 </template>
 
 <script lang="ts">
--- src/components/BaseFooter.vue
+++ src/components/BaseFooter.vue
@@ -1,20 +1,3 @@
 <template>
-	<footer class="footer">
-		<div class="container">
-			<p class="is-size-3">参考</p>
-			<p>こちらのサイトを参考に作らせていただきました。</p>
-			<ul>
-				<li>
-					<a href="https://ps2par-pnach.blogspot.com/"
-						>PS2PARコード→pnach用コード変換スクリプト</a
-					>
-				</li>
-				<li>
-					<a href="http://www.big.or.jp/~dram/ps2code.html"
-						>PS2PARコード変換スクリプト</a
-					>
-				</li>
-			</ul>
-		</div>
-	</footer>
+	<footer class="footer"></footer>
 </template>
