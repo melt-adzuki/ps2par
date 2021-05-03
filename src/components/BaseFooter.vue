b9723f0cba989804a553787843074f4a457a7ef3
hijiki02 <rebanira02@gmail.com>
2021-05-04 03:10:55 +0900

    Add a text to Footer

--- src/components/BaseFooter.vue
+++ src/components/BaseFooter.vue
@@ -1,3 +1,34 @@
 <template>
-	<footer class="footer"></footer>
+	<footer class="footer">
+		<small class="content has-text-centered is-size-6">
+			<p>
+				{{ currentYear }} Published by hijiki02 | Source code is available on
+				GitHub
+			</p>
+		</small>
+	</footer>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue"
+
+export default defineComponent({
+	setup(props) {
+		const currentYear = ref(0)
+
+		return {
+			currentYear
+		}
+	},
+	created() {
+		const dateObject = new Date()
+		this.currentYear = dateObject.getFullYear()
+	}
+})
+</script>
+
+<style lang="scss" scoped>
+.footer {
+	padding: 1.5rem;
+}
+</style>
