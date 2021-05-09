e983bac1cf81422a9ea4a03a6c250f8afdb90926
hijiki02 <rebanira02@gmail.com>
2021-05-10 00:05:10 +0900

    🚧 Comment out non-code

--- src/scripts/Convert.ts
+++ src/scripts/Convert.ts
@@ -75,65 +75,76 @@ export const Convert = {
 			binary = binary.trim()
 
 			if (!(binary === "")) {
-				if (binary.match(/^([89BF])/i)) {
-					if (!binary.match(/^\s*\/\/.*$/)) result += "//"
+				if (binary.match(/^\/\/.*$/) || "\n") {
 					result += binary
-				} else if (binary.match(/^\/\/.*$/)) {
-					result += binary
+				} else if (binary.match(/^([89BF])/i)) {
+					result += `//${binary}`
 				} else {
 					switch (mode) {
 						case "convert":
-							binary.match(/^([0-9A-F]{8})[\s　]*?([0-9A-F]{8})(.*)$/i)
-							result += "patch=1,EE,"
+							if (binary.match(/^([0-9A-F]{8})[\s　]*?([0-9A-F]{8})(.*)$/i)) {
+								result += "patch=1,EE,"
 
-							switch (RegExp.$1.charAt(0)) {
-								case "0":
-									result += `${RegExp.$1},byte,${RegExp.$2.substring(6, 8)}`
-									break
-								case "1":
-									result += `${RegExp.$1.substring(
-										1
-									)},short,${RegExp.$2.substring(4, 8)}`
-									break
-								case "2":
-									result += `${RegExp.$1.substring(1, 8)},word,${RegExp.$2}`
-									break
-								case "A":
-									result += `${RegExp.$1.substring(1, 8)},word,${RegExp.$2}`
-									break
-								case "F":
-									result += `${RegExp.$1.substring(1, 8)},word,${RegExp.$2}`
-									break
-								default:
-									result += `${RegExp.$1},extended,${RegExp.$2}`
+								switch (RegExp.$1.charAt(0)) {
+									case "0":
+										result += `${RegExp.$1},byte,${RegExp.$2.substring(6, 8)}`
+										break
+									case "1":
+										result += `${RegExp.$1.substring(
+											1
+										)},short,${RegExp.$2.substring(4, 8)}`
+										break
+									case "2":
+										result += `${RegExp.$1.substring(1, 8)},word,${RegExp.$2}`
+										break
+									case "A":
+										result += `${RegExp.$1.substring(1, 8)},word,${RegExp.$2}`
+										break
+									case "F":
+										result += `${RegExp.$1.substring(1, 8)},word,${RegExp.$2}`
+										break
+									default:
+										result += `${RegExp.$1},extended,${RegExp.$2}`
+								}
+							} else {
+								result += `//${binary}`
 							}
 
 							result += RegExp.$3
 
 						case "deconvert":
-							binary.match(
-								/^(\bpatch=[01],EE,\b)([0-9A-F]{7,8}),(\bbyte|short|word|extended\b),([0-9A-F]{2,8})(.*)$/i
-							)
+							if (
+								binary.match(
+									/^(\bpatch=[01],EE,\b)([0-9A-F]{7,8}),(\bbyte|short|word|extended\b),([0-9A-F]{2,8})(.*)$/i
+								)
+							) {
+								const cutAddress =
+									RegExp.$2.length === 8 ? RegExp.$2.substring(1, 8) : RegExp.$2
 
-							const cutAddress =
-								RegExp.$2.length === 8 ? RegExp.$2.substring(1, 8) : RegExp.$2
+								switch (RegExp.$3) {
+									case "byte":
+										result += `0${cutAddress} 000000${RegExp.$4}`
+										break
+									case "short":
+										result += `1${cutAddress} 0000${RegExp.$4}`
+										break
+									case "word":
+										result += `2${cutAddress} ${RegExp.$4}`
+										break
+									case "extended":
+										result += `${RegExp.$2} ${RegExp.$4}`
+										break
+								}
 
-							switch (RegExp.$3) {
-								case "byte":
-									result += `0${cutAddress} 000000${RegExp.$4}`
-									break
-								case "short":
-									result += `1${cutAddress} 0000${RegExp.$4}`
-									break
-								case "word":
-									result += `2${cutAddress} ${RegExp.$4}`
-									break
-								case "extended":
-									result += `${RegExp.$2} ${RegExp.$4}`
-									break
+								if (RegExp.$5 != "") {
+									const comment = RegExp.$5
+
+									comment.match(/(\s*)([^\s].*)$/)
+									result += `${RegExp.$1}//${RegExp.$2}`
+								}
+							} else {
+								result += `//${binary}`
 							}
-
-							result += RegExp.$5
 					}
 				}
 			}
