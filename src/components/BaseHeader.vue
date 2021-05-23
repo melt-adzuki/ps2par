deb1b319656e8838874d40e7219c4388c1ad9bc9
hijiki02 <rebanira02@gmail.com>
2021-05-23 17:16:49 +0900

    Add Logo.vue

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,9 +1,21 @@
 <template>
 	<header class="container text-center">
 		<h1 class="pb-4">
+			<logo />
 		</h1>
 		<h2 class="h5 font-weight-normal">
 			PS2PARコードを暗号化・複合化したり、PCSX2で使用できるpnachコードに変換したりできます。
 		</h2>
 	</header>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+import Logo from "./Logo.vue"
+
+export default defineComponent({
+	components: {
+		Logo
+	}
+})
+</script>
