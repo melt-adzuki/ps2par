48958d2fffe0d3aa0d018c945678c08d607bbdaa
hijiki02 <rebanira02@gmail.com>
2021-05-23 17:17:11 +0900

    Separate to ConvInput.vue

---
+++ src/components/ConvInput.vue
@@ -0,0 +1,21 @@
+<template>
+	<h3>{{ name }}</h3>
+	<textarea
+		:placeholder="placeholder"
+		rows="20"
+		:value="modelValue"
+		@input="$emit('update:modelValue', $event.target.value)"
+	></textarea>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+	props: {
+		name: String,
+		placeholder: String,
+		modelValue: String
+	}
+})
+</script>
--- src/components/Converter.vue
+++ src/components/Converter.vue
@@ -1,74 +1,60 @@
 <template>
 	<div class="container">
-		<div class="columns is-vcentered">
-			<div class="column">
-				<h3 class="is-size-3">暗号化</h3>
-				<textarea
-					class="textarea"
+		<div class="row row-align-items-start">
+			<div class="col">
+				<conv-input
+					name="暗号化"
 					placeholder="3C978328 1456E7A5"
-					rows="20"
 					v-model="encoded"
-				></textarea>
+				/>
 			</div>
-			<div class="column">
-				<div class="conv-container">
-					<conv-button
-						@click="decode"
-						icon="fa-arrow-right"
-						color="is-info"
-						class="mb-3"
-					>
-						複合化
-					</conv-button>
-					<conv-button @click="encode" icon="fa-arrow-left" color="is-danger">
-						暗号化
-					</conv-button>
-				</div>
+			<div class="col">
+				<conv-button
+					@click="decode"
+					icon="fa-arrow-right"
+					color="is-info"
+					class="mb-3"
+				>
+					複合化
+				</conv-button>
+				<conv-button @click="encode" icon="fa-arrow-left" color="is-danger">
+					暗号化
+				</conv-button>
 			</div>
-			<div class="column">
-				<h3 class="is-size-3">複合化</h3>
-				<textarea
-					class="textarea"
+			<div class="col">
+				<conv-input
+					name="複合化"
 					placeholder="00000000 00000000"
-					rows="20"
 					v-model="decoded"
-				></textarea>
+				/>
 			</div>
-			<div class="column">
-				<div class="conv-container">
-					<conv-button
-						@click="convertToPnach"
-						icon="fa-arrow-right"
-						color="is-primary"
-						class="mb-3"
-					>
-						変換
-					</conv-button>
-					<conv-button
-						@click="deconvertFromPnach"
-						icon="fa-arrow-left"
-						color="is-primary"
-						class="mb-3"
-					>
-						逆変換
-					</conv-button>
-					<conv-button
-						@click="copyPnach"
-						icon="fa-paperclip"
-						color="is-primary"
-					>
-						pnach<br />をコピー
-					</conv-button>
-				</div>
+			<div class="col">
+				<conv-button
+					@click="convertToPnach"
+					icon="fa-arrow-right"
+					color="is-primary"
+					class="mb-3"
+				>
+					変換
+				</conv-button>
+				<conv-button
+					@click="deconvertFromPnach"
+					icon="fa-arrow-left"
+					color="is-primary"
+					class="mb-3"
+				>
+					逆変換
+				</conv-button>
+				<conv-button @click="copyPnach" icon="fa-paperclip" color="is-primary">
+					pnach<br />をコピー
+				</conv-button>
 			</div>
-			<div class="column">
-				<h3 class="is-size-3">pnach</h3>
-				<textarea
-					class="textarea"
+			<div class="col">
+				<conv-input
+					name="pnach"
 					placeholder="patch=1,EE,00000000,byte,00"
-					rows="20"
 					v-model="pnach"
-				></textarea>
+				/>
 			</div>
 		</div>
 	</div>
@@ -77,6 +63,7 @@
 <script lang="ts">
 import { defineComponent } from "vue"
 import { useToast } from "vue-toastification"
+import ConvInput from "./ConvInput.vue"
 import ConvButton from "./ConvButton.vue"
 import { Convert } from "../scripts/Convert"
 
@@ -85,6 +72,7 @@ const toast = useToast()
 export default defineComponent({
 	name: "Converter",
 	components: {
+		ConvInput,
 		ConvButton
 	},
 	data() {
@@ -119,10 +107,3 @@ export default defineComponent({
 	}
 })
 </script>
-
-<style lang="scss" scoped>
-.conv-container {
-	width: 128px;
-	margin: auto;
-}
-</style>
