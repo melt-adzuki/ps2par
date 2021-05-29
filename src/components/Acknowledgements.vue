6eeefd5126ecb8d2d8db36d178cd2cb580a21e25
hijiki02 <rebanira02@gmail.com>
2021-05-30 01:01:07 +0900

    Use list rendering

--- src/components/Acknowledgements.vue
+++ src/components/Acknowledgements.vue
@@ -5,16 +5,34 @@
 			恐縮ながら、こちらのサイトを参考に制作させていただきました。ありがとうございました。
 		</p>
 		<ul>
-			<li>
-				<a href="https://ps2par-pnach.blogspot.com/"
-					>PS2PARコード→pnach用コード変換スクリプト</a
-				>
-			</li>
-			<li>
-				<a href="http://www.big.or.jp/~dram/ps2code.html"
-					>PS2PARコード変換スクリプト - zero memory</a
-				>
+			<li v-for="site in sites" :key="site.id">
+				<a :href="site.url" target="_blank" rel="noopener">
+					{{ site.title }}
+					<span v-if="site.author"> - {{ site.author }}</span>
+				</a>
 			</li>
 		</ul>
 	</div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+	setup() {
+		const sites = [
+			{
+				author: "zero memory",
+				title: "PS2PARコード変換スクリプト",
+				url: "http://www.big.or.jp/~dram/ps2code.html"
+			},
+			{
+				title: "PS2PARコード→pnach用コード変換スクリプト",
+				url: "https://ps2par-pnach.blogspot.com/"
+			}
+		]
+
+		return { sites }
+	}
+})
+</script>
