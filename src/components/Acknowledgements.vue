ca91686f359cb04c1dd156696a2b8494de2cc76a
hijiki02 <rebanira02@gmail.com>
2021-05-30 18:17:28 +0900

    Add third party notices

--- src/components/Acknowledgements.vue
+++ src/components/Acknowledgements.vue
@@ -1,17 +1,18 @@
 <template>
 	<div class="container">
 		<h2>謝辞</h2>
-		<p>
-			恐縮ながら、こちらのサイトを参考に制作させていただきました。ありがとうございました。
-		</p>
-		<ul>
-			<li v-for="site in sites" :key="site.id">
-				<a :href="site.url" target="_blank" rel="noopener">
-					{{ site.title }}
-					<span v-if="site.author"> - {{ site.author }}</span>
-				</a>
-			</li>
-		</ul>
+		<div v-for="site in sites" :key="site.id">
+			<h4>{{ site.title }}</h4>
+			<p v-if="site.description">{{ site.description }}</p>
+			<ul>
+				<li v-for="data in site.data" :key="data.id">
+					<a :href="data.url" target="_blank" rel="noopener">
+						{{ data.title }}
+						<span v-if="data.author"> - {{ data.author }}</span>
+					</a>
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
@@ -22,18 +23,39 @@ export default defineComponent({
 	setup() {
 		const sites = [
 			{
-				author: "かぢゃぽんのお部屋",
-				title: "PS2PAR解析部屋",
-				url: "http://www5.airnet.ne.jp/kajapon/"
+				title: "参考",
+				description:
+					"恐縮ながら、こちらのサイトを参考に制作させていただきました。ありがとうございました。",
+				data: [
+					{
+						author: "かぢゃぽんのお部屋",
+						title: "PS2PAR解析部屋",
+						url: "http://www5.airnet.ne.jp/kajapon/"
+					},
+					{
+						author: "zero memory",
+						title: "PS2PARコード変換スクリプト",
+						url: "http://www.big.or.jp/~dram/ps2code.html"
+					},
+					{
+						title: "PS2PARコード→pnach用コード変換スクリプト",
+						url: "https://ps2par-pnach.blogspot.com/"
+					}
+				]
 			},
 			{
-				author: "zero memory",
-				title: "PS2PARコード変換スクリプト",
-				url: "http://www.big.or.jp/~dram/ps2code.html"
-			},
-			{
-				title: "PS2PARコード→pnach用コード変換スクリプト",
-				url: "https://ps2par-pnach.blogspot.com/"
+				title: "サードパーティーライセンス",
+				data: [
+					{
+						author: "Ionic",
+						title: "Ionicons",
+						url: "https://ionic.io/ionicons"
+					},
+					{
+						title: "その他",
+						url: "./ThirdPartyNotices.txt"
+					}
+				]
 			}
 		]
 
