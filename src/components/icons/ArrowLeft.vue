cbdc38ed0e07ad60be5ff4ef910da39713e30f50
hijiki02 <rebanira02@gmail.com>
2021-05-30 20:38:35 +0900

    Fix: Invert the arrow icons

--- src/components/icons/ArrowLeft.vue
+++ src/components/icons/ArrowLeft.vue
@@ -1,13 +1,13 @@
 <template>
 	<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512">
-		<title>Arrow Forward</title>
+		<title>Arrow Back</title>
 		<path
 			fill="none"
 			stroke="currentColor"
 			stroke-linecap="round"
 			stroke-linejoin="round"
 			stroke-width="48"
-			d="M268 112l144 144-144 144M392 256H100"
+			d="M244 400L100 256l144-144M120 256h292"
 		/>
 	</svg>
 </template>
